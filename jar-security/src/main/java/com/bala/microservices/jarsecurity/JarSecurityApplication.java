83d9085f48d5fa51b899b33ff478031b23a5a224
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2021-04-19 09:01:38 +0300

    Initial Commit

---
+++ jar-security/src/main/java/com/bala/microservices/jarsecurity/JarSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.jarsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarSecurityApplication.class, args);
+	}
+
+}
---
+++ jar-security/src/test/java/com/bala/microservices/jarsecurity/JarSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.jarsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
