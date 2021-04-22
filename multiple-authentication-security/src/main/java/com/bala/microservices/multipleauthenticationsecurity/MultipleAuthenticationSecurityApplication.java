d9260b44c7b1910f01010905a9639edde01a2133
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2021-04-22 11:45:42 +0300

    Multiple Authentication Providers

---
+++ multiple-authentication-security/src/main/java/com/bala/microservices/multipleauthenticationsecurity/MultipleAuthenticationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.multipleauthenticationsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleAuthenticationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleAuthenticationSecurityApplication.class, args);
+	}
+
+}
---
+++ multiple-authentication-security/src/test/java/com/bala/microservices/multipleauthenticationsecurity/MultipleAuthenticationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.multipleauthenticationsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleAuthenticationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
