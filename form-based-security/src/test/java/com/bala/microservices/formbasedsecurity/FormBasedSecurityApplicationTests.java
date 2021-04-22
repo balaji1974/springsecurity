9a3a58fcf3efb648ea91e3b67a5ebfd6ee1f6fc1
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2021-04-22 11:49:24 +0300

    form based security - initial commit

---
+++ form-based-security/src/test/java/com/bala/microservices/formbasedsecurity/FormBasedSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.formbasedsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBasedSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
